d52e8ad6cce81fe9ab5e9758391aa1fd46ca57c9
asaulters <71015650+asaulters@users.noreply.github.com>
2021-07-05 20:58:25 -0400

    Add files via upload

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -6,11 +6,42 @@ mongoose
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true, minlength: 5, maxlength: 255 },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+    lowercase: true,
+    // uppercase:true
+    trim: true,
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (v, callback) {
+        setTimeout(() => {
+          //do some async work
+          const result = v && v.length > 0;
+          callback(result);
+        }, 4000);
+      },
+      message: "Course should have at least 1 tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+    get: (v) => Math.round(v),
+    set: (v) => Math.round(v),
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
@@ -18,26 +49,32 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
     name: "Node.js Course",
+    category: "Web",
     author: "Mosh",
-    tags: ["node", "backendend"],
+    tags: ["backend"],
     isPublished: true,
+    price: 15.8,
   });
 
-  const result = await course.save();
-  //   console.log(result);
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    for (field in ex.errors) console.log(ex.errors[field].message);
+  }
 }
 
 async function getCourses() {
   const pageNumber = 2;
   const pageSize = 10;
 
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
+  const courses = await Course.find({ _id: "60e2d6c7bba99846489e946c" })
+    // .skip((pageNumber - 1) * pageSize)
+    // .limit(pageSize)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    .select({ name: 1, tags: 1, price: 1 });
 
-  console.log(courses);
+  console.log(courses[0].price);
 }
 async function updateCourse(id) {
   // Approach: Quesry first
@@ -72,4 +109,4 @@ async function removeCourse(id) {
   const course = await Course.findByIdAndRemove(id);
   console.log(course);
 }
-removeCourse("60e1fdcbf520a879302ed853");
+getCourses();
