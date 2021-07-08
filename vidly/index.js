66d544a6200a0f8b0b746d2ed2e13c05022eb0c7
asaulters <71015650+asaulters@users.noreply.github.com>
2021-07-07 21:15:36 -0400

    Add files via upload

--- vidly/index.js
+++ vidly/index.js
@@ -1,11 +1,22 @@
-const Joi = require("joi");
+const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Oh no... Could not connect to MongoDB", err));
 
 app.use(express.json());
 app.use("/api/genres", genres); //for anything that starts with 1st param use second param path
+app.use("/api/customers", customers); //for anything that starts with 1st param use second param path
 // app.use("/", home); //for anything that starts with 1st param use second param path
+
+customers.router.post({ name: "Adam" }, { isGold: true }, { phone: 123456 });
+
+genres.router.post({ name: "Action" });
 
 //PORT
 const port = process.env.PORT || 3000;
